f47335a5edd7ec2947c29fe72aa00401bea38d61
Russell Omar <tom.jhon.1992@gmail.com>
2022-09-17 20:37:40 +0900

    correct

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 // const db = 'mongodb://127.0.0.1:27017/siren_db';
 // const db = 'mongodb+srv://brunokristo:HI7dEamVUCUIf4sw@cluster0.sgjetkr.mongodb.net/siren_cloud_db';
-const db = 'mongodb://localhost:27017/';
+const db = 'mongodb+srv://russellomar:pic19950918@cluster0.ivrpln7.mongodb.net/siren_cloud_db';
+// mongodb+srv://russellomar:<password>@cluster0.ivrpln7.mongodb.net/?retryWrites=true&w=majority
+// const db = 'mongodb://localhost:27017/';
 
 const connectDB = async () => {
   try {
