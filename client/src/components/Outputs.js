ed2f6f26518e7bf49b7e31c63dd45b129beb66ab
Russell Omar <tom.jhon.1992@gmail.com>
2022-10-25 16:35:39 +0900

    test4
    test4

--- client/src/components/Outputs.js
+++ client/src/components/Outputs.js
@@ -131,7 +131,7 @@ export default function Outputs() {
     let nameIdPairs = {};
     for (let i = 0; i < budgetlinesFiltered.length; i++)
       nameIdPairs[budgetlinesFiltered[i].name] = budgetlinesFiltered[i]._id;
-    const budgetLineIds = value.map((name) => nameIdPairs[name]).filter((option) => option !== null);
+    const budgetLineIds = value.map((name) => nameIdPairs[name]).filter((option) => option !== undefined);
     console.log("eagle: budgetLineIds.length2=", budgetLineIds);
     setConnectedBudgetLineIds(budgetLineIds);
   };
